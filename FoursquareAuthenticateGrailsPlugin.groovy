570dd32e26c9c837ebc47501bbe23ef9b6f45fd3
Eduard Martini <eduard.martini@gmail.com>
2012-11-26 10:09:36 +0100

    updated plugin descriptor

--- FoursquareAuthenticateGrailsPlugin.groovy
+++ FoursquareAuthenticateGrailsPlugin.groovy
@@ -6,11 +6,15 @@ class FoursquareAuthenticateGrailsPlugin {
     def title = "Foursquare Authenticate Plugin"
     def author = "Eduard Martini"
     def authorEmail = ""
-    def description = 'Brief summary/description of the plugin.'
+    def description = '''\
+    The Foursquare Platform is a set of APIs that make your application more social. Read more about integrating Foursquare with your web site on the Foursquare developer site.
+    This project contains the open source Grails Foursquare Authenticate Plugin that allows you to integrate the Foursquare Authenticate on a website/app powered by Grails.
+    I created this plugin because there are several plugins that handles the Foursquare API already, but none of them are taking care of the OAuth token aquisition.
+    '''
 
-    def documentation = "http://grails.org/plugin/foursquare-authenticate"
+    def documentation = "https://github.com/eduardm/fsq/wiki/Foursquare-Authenticate-Grails-plugin"
 
-//    def license = "APACHE"
+    def license = "APACHE"
     def issueManagement = [system: 'Github', url: 'https://github.com/eduardm/fsq/issues']
     def scm = [url: 'https://github.com/eduardm/fsq']
 
